82579563abf71bd301489e17acef8665f9b2bcfb
Miron <ms383504@students.mimuw.edu.pl>
2017-10-16 14:12:58 +0200

    Almost done. Chyba musze tylko błędy popoprawiać

--- main.cpp
+++ main.cpp
@@ -4,50 +4,105 @@
 #define WRONG_FILE 1
 #define WRONG_NUMBER_OF_PARAMETERS 2
 #define WRONG_ID 3
+#define WRONG_GROUP_DESCRIPTION 4
+#define ID_DUPLICATE 5
 #define MAX_GROUP_SIZE 3
 #define MIN_GROUP_SIZE 2
-//#define
+
 using namespace std;
 
-
-void handleError(int errorType, int inLine, string fileName = ""){
-
+void handleError(int errorType, int lineNumber, string message = "MISSING_ERROR_PARAMETER", string message2 = "MISSING_ERROR_PARAMETER2"){
+//    ostream errorOutput = cout;
+    if(errorType == WRONG_LINE_IN_INPUT_FILE)
+        cout << "Error in " + message + ", line " << lineNumber << ": " + message2 + "\n";
+    else if(errorType == WRONG_FILE)
+        cout << "Error: problem with file " + message + "\n";
+    else if(errorType == WRONG_NUMBER_OF_PARAMETERS)
+        cout << "Usage: " + message + " file\n";
+    else if(errorType == ID_DUPLICATE || errorType == WRONG_ID)
+        cout << "Error in " + message + ", line " << lineNumber << ": " + message2 + "\n";
+    else if(errorType == WRONG_GROUP_DESCRIPTION)
+        cout << "Error in cin, line " << lineNumber << ": " + message + "\n";
+    else
+        cout << "Unknown error happened in line " << lineNumber << " error id: "
+                    << errorType << " error massages: " + message + " " +message2 + "\n";
 }
 
 bool isIdCorrect(string studentId){
-    regex idRegex("([a-z]{2}[0-9]{6})|([a-z]{3}-[0-9]{4})");
+    regex idRegex("([a-z]{2}[0-9]{6})|([a-z]{2}m-[0-9]{4})");
     return regex_match(studentId,idRegex);
 }
 
-list<string> splitString(const string& str, const string& separator){
+list<string> splitString(string& str, int lineNumber){
     list<string> result;
-    //todo
+    while(str.length() >= 8){
+        result.push_back(str.substr(0,8));
+        if(str[8] != '+' && str.length() > 8)
+            handleError(WRONG_GROUP_DESCRIPTION,lineNumber);
+        str = str.substr(8 + (str.length() > 10));
+    }
+    if(str.length() != 0 ){
+        handleError(WRONG_GROUP_DESCRIPTION,lineNumber);
+        result.clear();
+    }
     return result;
 }
-//poprawne istniejące id, bez powtórzeń
+//to jest trochę za długie chyba
 list<string> ExtractStudentsFromGroup(string groupDescription, int lineNumber, map<string,vector<string>>& existingGroups) {
     regex prefixRegex("grupa[1-8]/zadanie[1-6]/");
     list<string> result;
-
+    string groupDescriptionCopy = groupDescription;
+    set<string> idsInGroup;
+    list<list<string>::iterator> stringToDelete;
     if(regex_search(groupDescription,prefixRegex)){
-        groupDescription=regex_replace(groupDescription,prefixRegex,"");
+         groupDescription = regex_replace(groupDescription,prefixRegex,"");
     } else {
-        //TODO obsługa błędu wejścia
+        handleError(WRONG_GROUP_DESCRIPTION,lineNumber,groupDescriptionCopy);
+        result.clear();
+        return result;
     }
-    result = splitString(groupDescription,"+");
+
+    result = splitString(groupDescription,lineNumber);
+    if(result.size() == 0)
+        return result;
+
+    if(result.size() < MIN_GROUP_SIZE || result.size() > MAX_GROUP_SIZE){
+        handleError(WRONG_GROUP_DESCRIPTION,lineNumber,groupDescriptionCopy);
+        result.clear();
+        return result;
+    }
+
+    for (list<string>::iterator it = result.begin(); it != result.end(); it++) {
+        if (isIdCorrect(*it) == false) {
+            handleError(WRONG_GROUP_DESCRIPTION,lineNumber,groupDescriptionCopy);
+            result.clear();
+            return result;
+        }
+    }
 
     for (list<string>::iterator it = result.begin(); it != result.end(); it++){
-        if(isIdCorrect(*it)){
-        } else {
-            //TODO obsługa błędu wejścia
+        if(existingGroups.find(*it) == existingGroups.end()){
+            handleError(WRONG_ID,lineNumber,*it);
+            stringToDelete.push_back(it);
         }
+        else {
+            if(idsInGroup.find(*it) == idsInGroup.end()){
+                idsInGroup.insert(*it);
+            } else{
+                handleError(WRONG_ID,lineNumber,*it);
+                stringToDelete.push_back(it);
+            }
+        }
+    }
+
+    for(list<list<string>::iterator>::iterator it  = stringToDelete.begin(); it != stringToDelete.end(); it++){
+        result.erase(*it);
     }
 
     return result;
 }
 
-pair<vector<string>, bool> readStudentsIdsFromFile(string fileName){
-    set<string> existingIds;
+pair<vector<string>, bool> readStudentsIdsFromFile(string fileName, map<string,vector<string>>& existingGroups){
     vector<string> input;
     pair<vector<string>,bool> result;
     result.second = true;
@@ -56,13 +111,18 @@ pair<vector<string>, bool> readStudentsIdsFromFile(string fileName){
     int lineNumber = 1;
 
     if(inputStream.is_open()){
-        getline(inputStream,bufor);
+        getline(cin,bufor);
         while(bufor != ""){
-            if(isIdCorrect(bufor) && existingIds.find(bufor) != existingIds.end()) {
+            if(isIdCorrect(bufor) && existingGroups.find(bufor) == existingGroups.end()) {
                 result.first.push_back(bufor);
-                existingIds.insert(bufor);
+                pair<string,vector<string>> help;
+                help.first = bufor;
+                vector<string> emptyVector;
+                emptyVector.clear();
+                help.second = emptyVector;
+                existingGroups.insert(help);
             } else {
-                handleError(WRONG_LINE_IN_INPUT_FILE,lineNumber,fileName);
+                handleError(WRONG_LINE_IN_INPUT_FILE,lineNumber,fileName,bufor);
             }
             lineNumber++;
             bufor.clear();
@@ -72,6 +132,7 @@ pair<vector<string>, bool> readStudentsIdsFromFile(string fileName){
         handleError(WRONG_FILE,0,fileName);
         result.second = false;
     }
+
     return result;
 }
 
@@ -102,14 +163,13 @@ int main(int argc,  char** argv ) {
         handleError(WRONG_NUMBER_OF_PARAMETERS,0);
         return 1;
     } else {
-        pair<vector<string>,bool> studentsIds = readStudentsIdsFromFile(argv[1]);
+        pair<vector<string>,bool> studentsIds = readStudentsIdsFromFile(argv[1],existingGroups);
         if(studentsIds.second == false){
             return 1;
         } else {
             list<list<string>> groups = readGroupDescriptionsFromStdin(existingGroups);
         }
     }
-
 
     return 0;
 }
